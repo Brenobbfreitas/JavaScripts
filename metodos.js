be4e47d465cd1f6fa848048cf7265a21f45d1e94
brenobbfreitas <brenobbfreitas@gmail.com>
2021-08-24 05:43:46 -0300

    add trim  metodo

--- metodos.js
+++ metodos.js
@@ -13,3 +13,21 @@ console.log(strCopy);
 // expected output: Array ["The quick brown fox jumps over the lazy dog."]
 
 //metodo split -->> divide a string em uma cadeia de arrays
+
+
+
+
+
+//metodo trim -->> remove os espaços na string
+
+
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// expected output: "Hello world!";
+
+String.prototype.trimStart()
+String.prototype.trimEnd()
