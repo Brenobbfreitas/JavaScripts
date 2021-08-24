8917c2872931b3194e2328c401ef75b404e80322
brenobbfreitas <brenobbfreitas@gmail.com>
2021-08-24 01:14:13 -0300

    add o for e sua estrutura

--- 8-loops.js
+++ 8-loops.js
@@ -8,30 +8,37 @@ const listaDeDestinos = new Array(
 
 const idade = 18;
 const estaAcompanhada = false;
-let temPassagemComprada = false;
-const destino = `curitiba`;
+let temPassagemComprada = false; //variavel mutalvel
+const destino = `curitiba`; //variavel constante
 
 console.log("Locais Disponiveis: \n")
 console.log(listaDeDestinos);
 
-const podeComprar = idade >= 18 || estaAcompanhada == true;
+const podeComprar = idade >= 18 || estaAcompanhada == true; //condicional ou
 
 let contador = 0;
 let destinoExiste = false
-while(contador < 4){
+while(contador < 4){ //repetidor
     if(listaDeDestinos[contador] == destino){
         destinoExiste = true;
         temPassagemComprada = true;
         console.log(`Compra realizada !! Destino Agendado: ${listaDeDestinos[contador]}`);
-        break;
+        break; //interrompe a operação
     }
     contador += 1;
 }
 
-console.log(`ticket:${temPassagemComprada}`);
+console.log(`ticket:${temPassagemComprada}`); // value boolean
 
 if(destinoExiste == true && podeComprar == true){
     console.log(`Boa viagem`)
 }else{
     console.log(`desculpe temos um erro`)
 }
+
+
+for(let i  = 0 ; i < 3 ; i++){
+    if(listaDeDestinos[i] == destino){
+        destinoExiste = true;
+    }
+} // repetidor for
