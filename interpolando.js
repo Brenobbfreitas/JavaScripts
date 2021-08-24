43cade294c0fed8f3d2077d44ca55b0a84c85a83
brenobbfreitas <brenobbfreitas@gmail.com>
2021-08-24 06:06:26 -0300

    interpolando parametros function

---
+++ interpolando.js
@@ -0,0 +1,10 @@
+
+
+
+minhaFunction = (arrayDeStrings, palavra) =>{
+    console.log(`${arrayDeStrings[0]}${palavra}${arrayDeStrings[1]}`);
+}
+
+const palavra = 'alura';
+
+minhaFunction`Eu estudo na ${palavra} todos os dias`; //parametros para a função minhaFunction o ${palavra} separa os parametros
