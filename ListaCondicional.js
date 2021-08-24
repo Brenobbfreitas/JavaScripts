ea55b6edaa0596c65cdbbb62733eb0e70769ce6f
brenobbfreitas <brenobbfreitas@gmail.com>
2021-08-24 01:08:40 -0300

    Algumas condicionais JS

---
+++ ListaCondicional.js
@@ -0,0 +1,47 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else if (estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else {
+//     console.log("Não é maior de Idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Passagen comprada com sucesso!!"); 
+    listaDeDestinos.splice(2, 1); //removendo item
+    temPassagemComprada = true;
+} else {
+    console.log("Não é maior de Idade e não posso vender");
+}
+
+console.log("Embarque: \n")
+if(temPassagemComprada == true){
+    console.log("Boa viagem");
+}else{
+    console.log("Você não pode embarcar");
+}
+
+
+console.log(`Lista de destinos ${listaDeDestinos}`);
+
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
