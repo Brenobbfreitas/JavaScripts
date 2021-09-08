7e913e978f0faf45dc8aa819a3e40ccba786a025
brenobbfreitas <brenobbfreitas@gmail.com>
2021-09-08 01:57:37 -0300

    calc imc from dom

---
+++ JavaScript/introducao-javascript/js/principal.js
@@ -0,0 +1,21 @@
+			//As funções devem estar dentro da tag script, para ser interpretada pelo navegador
+			//A função querySelector é utilizada para pegar os valores da tag e posteriormente manipula
+            //O documente é por onde acessamos a DOM que contem o HTML da pagina
+			var titulo = document.querySelector(".titulo")
+			titulo.textContent = "Aparecida Nutricionista";
+
+            var paciente  = document.querySelector("#primeiro-paciente");
+
+            var tdPeso = paciente.querySelector(".info-peso");
+            var peso = tdPeso.textContent
+
+            var tdAltura = paciente.querySelector(".info-altura");
+            var  altura = tdAltura.textContent //textContent busca o valor text
+
+
+            var tdImc = paciente.querySelector(".info-imc");
+
+            var imc =  peso / (altura * altura);
+
+            tdImc.textContent = imc
+            //IMC VARIAVEL
