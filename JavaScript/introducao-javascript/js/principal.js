a3ca29513daf29d13963386b4d002f8c8263f4e8
brenobbfreitas <brenobbfreitas@gmail.com>
2021-09-13 17:35:46 -0300

    add propriedade classList e o metodo add()

--- JavaScript/introducao-javascript/js/principal.js
+++ JavaScript/introducao-javascript/js/principal.js
@@ -7,6 +7,8 @@
             var pacientes  = document.querySelectorAll(".paciente");
             //acessa todos os valores com class paciente
 
+
+
             for(var i = 0; i < pacientes.length; i++){
                 paciente = pacientes[i]
 
@@ -28,20 +30,23 @@
                 // add condicionais
                 if(peso <= 0 || peso >= 1000){
                     console.log("peso inválido");
-                    pesoEhValido = false
-                    tdImc.textContent = "Peso Inválido"
+                    pesoEhValido = false;
+                    tdImc.textContent = "Peso Inválido";
+                    paciente.classList.add("paciente-invalido")
+                    //paciente.style.backgroundColor = "lightcoral"; //style modifica o "css" da pagina
                 }
     
                 if(altura <= 0 || altura >= 3.00){
                     console.log("Altura invalida");
-                    alturaEhValida = false
-                    tdImc.textContent = "Altura Inválida"
+                    alturaEhValida = false;
+                    tdImc.textContent = "Altura Inválida";
+                    paciente.classList.add("paciente-invalido");//retorna todas as classes do objeto e alguns metodos -> add.()
                 }
                 
                 //altera a dom(html da pagina)
                 if(alturaEhValida && pesoEhValido ){
                     var imc =  peso / (altura * altura);
-                    tdImc.textContent = imc
+                    tdImc.textContent = imc.toFixed(2);//A função toFixed limita as casas decimais
                 }
             }
 
